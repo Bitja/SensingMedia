050d8272bca416dbe423fecbf926e88bb10ddb67
Mette Jakobsen <mette9jakobsen@homail.com>
2015-09-17 13:08:26 +0200

    Added folders for prefabs, materials etc. The cube and the canvas can be deleted if it doesn't fit what you're working on.

---
+++ Sensing Media/Assets/Scripts/lampGlow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lampGlow : MonoBehaviour {
+
+	int intensity = 0; //0-255
+	int t = 1 / intensity; //0-1
+	public Color color0 = Color.white;
+	public Color color1 = Color.black;
+	public Shader lerpedColor;
+
+	void Start () {
+		lerpedColor = GetComponent<Shader>();
+	}
+	
+	void Update () {
+		lerpedColor.Albedo = Color.Lerp(Color.white, Color.black, t);
+	}
+}
