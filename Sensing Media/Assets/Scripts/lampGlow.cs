98d753822368939ddd3cf06aa623a6451a9413ba
Mette Jakobsen <mette9jakobsen@homail.com>
2015-09-22 13:40:50 +0200

    Made color change with slider in scene 1

--- Sensing Media/Assets/Scripts/lampGlow.cs
+++ Sensing Media/Assets/Scripts/lampGlow.cs
@@ -1,17 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class lampGlow : MonoBehaviour {
-	int intensity = 10;
-	int timyTime = 30;
-	float fuckmylife =42;
-	// Use this for initialization
+
+	public Color colorStart = Color.white;
+	public Color colorEnd = Color.black;
+	public float duration = 1.0F;
+	public Renderer rend;
+	public float intencity;
+
 	void Start () {
+
+		rend = GetComponent<Renderer>();
+
+	}
+	public void LightIntencity(float newIntencity) {
+		intencity = newIntencity;
+		Debug.Log (intencity);
+
+
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		//float intencity = Mathf.PingPong(Time.time, duration) / duration;
+		rend.material.color = Color.Lerp(colorStart, colorEnd, intencity);
+	//	Debug.Log (intencity);
+
+
 	}
 }
