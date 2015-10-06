ebdccaf9b62c66e90be3d40ec59afba6f85b9ea5
Daniel Svejstrup Christensen <daniel@drakkashi.com>
2015-10-06 13:55:52 +0200

    Circle script etc.

---
+++ Sensing Media/Assets/Scripts/Circle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Circle : MonoBehaviour {
+
+    public static bool mouseEnabled = true;
+
+    public int id;
+
+    private Color defaultColour;
+    private Color selectedColour = Color.red;
+    private Material mat;
+
+	void Start () {
+        mat = GetComponent<SpriteRenderer>().material;
+        defaultColour = mat.color;
+    }
+
+    void OnTouchDown() {
+        if (id == 1)
+        {
+            Handler.prepare();
+            mouseEnabled = false;
+        }
+        else if (id == 2)
+            Handler.end();
+        mat.color = selectedColour;
+    }
+    void OnTouchExit() {
+        if (id == 0 && mouseEnabled)
+            Handler.start();
+        mat.color = defaultColour;
+    }
+
+    void Update()
+    {
+        mouseEnabled = true;
+    }
+}
---
+++ Sensing Media/Assets/Scripts/Handler.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System;
+
+public class Handler : MonoBehaviour {
+
+    private static int stage = 0;
+    private static int state = 0;
+    private static long timestampBeg = 0;
+    private static long timestampEnd = 0;
+    private static int countStart = 1;
+    private static int countEnd = 0;
+    private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    private static data[] dataList = new data[5];
+
+    public struct data
+    {
+        public long millis;
+        public float accuracy;
+        public int count;
+    }
+
+    public static void prepare()
+    {
+        if (state == 0)
+        {
+            state++;
+            Debug.Log("Preparing for Start");
+        }
+    }
+
+    public static void start()
+    {
+        if (state == 1)
+        {
+            state++;
+            timestampBeg = getMillis();
+            Debug.Log("Starting");
+        }
+    }
+
+    public static void end()
+    {
+        if (state == 2)
+        {
+            state++;
+            timestampEnd = getMillis();
+
+            dataList[stage] = new data();
+            dataList[stage].millis = timestampEnd - timestampBeg;
+            dataList[stage].accuracy = countEnd / countStart;
+            dataList[stage].count = countEnd;
+
+            Debug.Log("Ending");
+            Debug.Log("Time: " + (dataList[stage].millis / 1000.0f));
+        }
+    }
+
+    public static long getSeconds()
+    {
+        return (long)(DateTime.UtcNow - UnixEpoch).TotalSeconds;
+    }
+
+    public static long getMillis()
+    {
+        return (long)(DateTime.UtcNow - UnixEpoch).TotalMilliseconds;
+    }
+}
