8a5b2782718404e6977f02dd8783fd399e36bbdb
Daniel Svejstrup Christensen <daniel@drakkashi.com>
2015-10-14 08:55:19 +0200

    Resolved merge errors

--- Sensing Media/Assets/Scripts/PathTracer.cs
+++ Sensing Media/Assets/Scripts/PathTracer.cs
@@ -30,84 +30,36 @@ public class PathTracer : MonoBehaviour {
         
         if (Input.GetMouseButton(0)) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Send a ray to collide with the plane
-<<<<<<< HEAD
-
-            if (GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity)) {
-=======
-            
-            /*
-                // planeUI toggle
-                if (isOn) {
-                    Vector3 toggleOut = new Vector3(-3.5f, 0, 0);
-                    UIobj.transform.position += toggleOut;
-                    isOn = false;
-                    Debug.Log("false");
-                }
-                else if (!isOn && hit.transform.name == "UIobj") {
-                    Debug.Log("true");
-                    Vector3 toggleIn = new Vector3(3.5f, 0, 0);
-                    UIobj.transform.position += toggleIn;
-                    isOn = true;
-                }
-                */
 
             if (GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity)) { // change
->>>>>>> d87bdd93bda4c40b63686332e517fd8cdb7aec0b
                 Vector2 uv;
                 uv.x = (hit.point.x - hit.collider.bounds.min.x) / hit.collider.bounds.size.x;
                 uv.y = (hit.point.y - hit.collider.bounds.min.y) / hit.collider.bounds.size.y;
                 Texture2D tex = (Texture2D)hit.transform.gameObject.GetComponent<Renderer>().sharedMaterial.mainTexture;
                 float x = uv.x * tex.width;
                 float y = uv.y * tex.height;
-<<<<<<< HEAD
 
                 if (preX >= 0 && (preX != x || preY != y)) { 
                     int width = 15;
                     nearestP = width;
-          
+
                     for (int i = -width / 2; i <= width / 2; i++)
-                        for (int j = -width / 2; j <= width / 2; j++) {
+                        for (int j = -width / 2; j <= width / 2; j++)
+                        {
                             float pDistance = Mathf.Sqrt(Mathf.Pow(i, 2) + Mathf.Pow(j, 2));
-                            if (pDistance <= width / 2.0) {
+                            if (pDistance <= width / 2.0)
+                            {
                                 Color pColor = tex.GetPixel((int)(x + i), (int)(y + j));
-                                if (pColor == Color.white || pColor == Color.red) {
+                                if (pColor == Color.white || pColor == Color.red)
+                                {
                                     if (pColor == Color.white)
                                         tex.SetPixel((int)(x + i), (int)(y + j), Color.red);
                                     if (nearestP > pDistance)
                                         nearestP = pDistance;
-=======
-                Vector2 point = new Vector2(x, y);
-
-                if (preX >= 0) {
-                    Vector2 prePoint = new Vector2(preX, preY);
-                    Vector2[] points = new Vector2[2];
-                    points[0] = prePoint;
-                    points[1] = point;
-                    points = MakeSmoothCurve(points, 100.0f);
-
-                    int kernel = 15;
-                    nearestP = kernel;
-                    for (int p = 0; p < points.Length; p++) {
-                        for (int i = -kernel / 2; i <= kernel / 2; i++)
-                            for (int j = -kernel / 2; j <= kernel / 2; j++) {
-                                float pDistance = Mathf.Sqrt(Mathf.Pow(i, 2) + Mathf.Pow(j, 2));
-                                if (pDistance <= kernel / 2.0) {
-                                    Color pColor = tex.GetPixel((int)(points[p].x + i), (int)(points[p].y + j));
-                                    if (pColor == Color.white || pColor == Color.red) {
-                                        if (pColor == Color.white)
-                                            tex.SetPixel((int)(points[p].x + i), (int)(points[p].y + j), Color.red);
-                                        if (nearestP > pDistance)
-                                            nearestP = pDistance;
-                                    }
-                                    else
-                                        tex.SetPixel((int)(points[p].x + i), (int)(points[p].y + j), Color.blue);
->>>>>>> d87bdd93bda4c40b63686332e517fd8cdb7aec0b
                                 }
-                                else
-                                    tex.SetPixel((int)(x + i), (int)(y + j), Color.blue);
                             }
                         }
-
+  
                     nearestP /= width;
 
                     float length = Mathf.Sqrt(Mathf.Pow(Mathf.Abs(preX - x), 2) + Mathf.Pow(Mathf.Abs(preY - y), 2));
