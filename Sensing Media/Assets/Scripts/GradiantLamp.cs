ed4610676b58bbe8f6dff3715b747f4f350fb3c2
Mette Jakobsen <mette9jakobsen@homail.com>
2015-10-17 16:36:21 +0200

    Lamp glow effect done.

--- Sensing Media/Assets/Scripts/GradiantLamp.cs
+++ Sensing Media/Assets/Scripts/GradiantLamp.cs
@@ -7,45 +7,55 @@ public class GradiantLamp : MonoBehaviour {
 
     public Color colorStart;
     public Color colorMiddle;
+	public Color colorMiddle2;
     public Color colorEnd;
     private Renderer rend;
-    public float intencity;
     public bool isOn = true;
-    private GameObject distance01;
+ 
+
+	private int unitNumber; 
+	private float closestDistance = 200;
+	private float[] unitDistances = new float[17];//remember to change this to number of squares
+	float distance;
     private GameObject mouse;
     private float newLerb;
-    public float drawDistance = 5; // middle colour
-    public Vector3[] distanceBoxes = new Vector3[3];
-    //private float distance;
+	public List<GameObject> distanceUnits= new List<GameObject>();
+    
 
     public void toggle(bool b) {
         isOn = b;
     }
     void Start() {
         rend = GetComponentInChildren<Renderer>();
-        distance01 = GameObject.Find("Distance01");
+        
         mouse = GameObject.Find("MouseGradient");
+
     }
 
     void Update() {
-        rend.enabled = false;
-        
-        if (isOn && PathTracer.nearestP <= 0.99) {
-            rend.enabled = true;
-            rend.material.color = Color.Lerp(colorStart, colorMiddle, PathTracer.nearestP);
-            //Debug.Log("<0.99" + rend.material.color);
-        }
-        else if (isOn && PathTracer.nearestP >= 1) {
-            /*for (int i = 0; i < distanceBoxes.Length; i++) {
-                distance01.transform.position = distanceBoxes[i];
-                distance = Vector3.Distance(mouse.transform.position, distance01.transform.position);
-            }*/
+		rend.enabled = false;
+	
+		if (isOn && PathTracer.nearestP < 1) {
+			rend.enabled = true;
+			rend.material.color = Color.Lerp (colorStart, colorMiddle, PathTracer.nearestP);
+		} 
+		else if (isOn && PathTracer.nearestP >= 1) {
+			for (int i = 0; i < distanceUnits.Count; i++) {
+				unitDistances[i] = Vector3.Distance(mouse.transform.position, distanceUnits[i].transform.position);				 
+			}
+			for (int i = 0; i < unitDistances.Length; i++) {
+				if (unitDistances [i] < closestDistance) {
+					closestDistance = unitDistances[i];
+					unitNumber = i;//for debugging: delete when done
+				}
+			}
+			newLerb = closestDistance/1.8f;
+			Debug.Log (closestDistance + ":" + unitNumber+ ":" + newLerb);
+			rend.enabled = true;
+			rend.material.color = Color.Lerp (colorMiddle2, colorEnd,newLerb);
+			closestDistance = 200;
+			//Debug.Log(">1"+rend.material.color);
+		}
 
-            float distance = Vector3.Distance(mouse.transform.position, distance01.transform.position);
-            newLerb = distance / drawDistance;
-            rend.enabled = true;
-            rend.material.color = Color.Lerp(colorMiddle, colorEnd, PathTracer.nearestP);
-            //Debug.Log(">1"+rend.material.color);
-        }
-    }
+	}
 }
