6873b6b45df96688b28a2720bc418fd16de50a14
Mette Jakobsen <mette9jakobsen@homail.com>
2015-10-21 13:59:15 +0200

    Made the first 2 effects for the line test: -Show nothing -Show line in top corner

---
+++ Sensing Media/Assets/Scripts/TogglePlanes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TogglePlanes : MonoBehaviour {
+
+	public Image plane;
+
+
+	void Start(){
+
+		plane.enabled = false;
+
+	}
+	public void PlaneShow() {	
+		plane.enabled = true;
+	}
+	public void PlaneHide() {	
+		plane.enabled = false;
+		
+		
+	}
+}
