fd6bb0e0e64990d30be5c459d24ae3312d8bca22
Mark Kronborg Poulsen <kronborg-poulsen@hotmail.com>
2015-11-20 15:06:00 +0100

    Logging

---
+++ Sensing Media/Assets/Scripts/Logging.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Logging : MonoBehaviour
+{
+    public string[] LabelsA;
+    string print;
+    string filename = "text1.txt";
+    var documents = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        using(	System.IO.StreamWriter file = new System.IO.StreamWriter(documents + @"/" + filename,true))
+        	{
+
+        		for(int i =0; i < LabelsA.Length; i++)
+        		{
+        		print = LabelsA[i];
+        		file.WriteLine(print);
+        		}
+        	}
+
+    }
+}
