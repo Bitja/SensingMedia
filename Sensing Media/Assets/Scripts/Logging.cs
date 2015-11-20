e9e5b4ad1d230e9203c461b0b783a3dd6bc85604
Mette Jakobsen <mette9jakobsen@homail.com>
2015-11-20 17:57:12 +0100

    Data collection stuff

--- Sensing Media/Assets/Scripts/Logging.cs
+++ Sensing Media/Assets/Scripts/Logging.cs
@@ -1,31 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.IO;
+using UnityEngine.UI;
 
-public class Logging : MonoBehaviour
-{
-    public string[] LabelsA;
-    string print;
-    string filename = "text1.txt";
-    var documents = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
-    // Use this for initialization
-    void Start()
-    {
+public class Logging : MonoBehaviour {
+	public GameObject MyText;
+	private Text VisibleText;
 
-    }
+	void Start(){
 
-    // Update is called once per frame
-    void Update()
-    {
-        using(	System.IO.StreamWriter file = new System.IO.StreamWriter(documents + @"/" + filename,true))
-        	{
+		VisibleText = MyText.GetComponent<Text> ();
+	}
+	void Update(){
+		string fileName = "text1.txt";
+		string path = Application.persistentDataPath + "/" + fileName;
+		VisibleText.text = path;
+		using (System.IO.StreamWriter sw = new System.IO.StreamWriter (path, true)) {
+			sw.WriteLine ("Hello World");
+		}
+		Debug.Log (VisibleText.text);
+	}
 
-        		for(int i =0; i < LabelsA.Length; i++)
-        		{
-        		print = LabelsA[i];
-        		file.WriteLine(print);
-        		}
-        	}
-
-    }
 }
