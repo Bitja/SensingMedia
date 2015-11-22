b64e22bb000a15761e73ba47e0ca394bdf13e156
Mette Jakobsen <mette9jakobsen@homail.com>
2015-11-22 11:26:18 +0100

    Logging can write and read from file

--- Sensing Media/Assets/Scripts/Logging.cs
+++ Sensing Media/Assets/Scripts/Logging.cs
@@ -4,6 +4,8 @@ using System;
 using System.IO;
 using UnityEngine.UI;
 
+
+
 public class Logging : MonoBehaviour {
 	public GameObject MyText;
 	private Text VisibleText;
@@ -13,13 +15,18 @@ public class Logging : MonoBehaviour {
 		VisibleText = MyText.GetComponent<Text> ();
 	}
 	void Update(){
-		string fileName = "text1.txt";
+		string fileName = "text2.txt";
 		string path = Application.persistentDataPath + "/" + fileName;
 		VisibleText.text = path;
 		using (System.IO.StreamWriter sw = new System.IO.StreamWriter (path, true)) {
-			sw.WriteLine ("Hello World");
+			for(int i =0 ; i<=10; i++)
+				sw.WriteLine (i);
 		}
+
+		using (System.IO.StreamReader sr = new System.IO.StreamReader (path, true)) {
+			VisibleText.text = sr.ReadLine();
 		Debug.Log (VisibleText.text);
 	}
 
+}
 }
